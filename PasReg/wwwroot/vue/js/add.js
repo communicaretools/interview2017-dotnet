0bd96081b50d3f54053c665da43ff04ff042449b
Stein Jakob Nordbø <steinjak@gmail.com>
2017-06-06 13:29:01 +0200

    Added skeleton for add component

--- PasReg/wwwroot/vue/js/add.js
+++ PasReg/wwwroot/vue/js/add.js
@@ -1,3 +1,23 @@
 const Add = {
-    template: '<p>add view</p>'
+    template: `
+    <div>
+        <form>
+            <div class="form-group">
+                This is the add registration form
+            </div>
+            <button type="submit" class="btn btn-primary" v-on:click="submit()">Submit</button>
+            <router-link to="/list" class="btn btn-default">Cancel</router-link>
+        </form>
+    </div>
+    `,
+    data: () => {
+        return {
+            registration: {}
+        };
+    },
+    methods: {
+        submit: function submit() {
+            console.log('Ready to submit registration', this.registration);
+        }
+    }
 };
